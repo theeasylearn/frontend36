0709445f72052529967346f2f29b1378ff61f325
theeasylearn <theeasylearn@gmail.com>
2024-07-13 09:56:11 +0530

    lesson 28

---
+++ javascript/data.js
@@ -0,0 +1,323 @@
+[
+    {
+        "name": "Casual Shirt",
+        "category": "Clothing",
+        "price": 1523,
+        "color": "blue",
+        "brand": "Friskers",
+        "photo": "https://example.com/photos/casual_shirt.jpg"
+    },
+    {
+        "name": "Running Shoes",
+        "category": "Footwear",
+        "price": 2789,
+        "color": "green",
+        "brand": "HRX by Hrithik Roshan",
+        "photo": "https://example.com/photos/running_shoes.jpg"
+    },
+    {
+        "name": "Formal Pants",
+        "category": "Clothing",
+        "price": 1899,
+        "color": "black",
+        "brand": "Roadster",
+        "photo": "https://example.com/photos/formal_pants.jpg"
+    },
+    {
+        "name": "Leather Jacket",
+        "category": "Clothing",
+        "price": 4550,
+        "color": "red",
+        "brand": "Tommy Hilfiger",
+        "photo": "https://example.com/photos/leather_jacket.jpg"
+    },
+    {
+        "name": "Wrist Watch",
+        "category": "Accessories",
+        "price": 3199,
+        "color": "pink",
+        "brand": "V-Mart",
+        "photo": "https://example.com/photos/wrist_watch.jpg"
+    },
+    {
+        "name": "Sports Cap",
+        "category": "Accessories",
+        "price": 850,
+        "color": "white",
+        "brand": "WROGN",
+        "photo": "https://example.com/photos/sports_cap.jpg"
+    },
+    {
+        "name": "Denim Jeans",
+        "category": "Clothing",
+        "price": 2325,
+        "color": "gray",
+        "brand": "U.S. Polo Assn.",
+        "photo": "https://example.com/photos/denim_jeans.jpg"
+    },
+    {
+        "name": "Sneakers",
+        "category": "Footwear",
+        "price": 3990,
+        "color": "navy blue",
+        "brand": "VIMAL JONNEY",
+        "photo": "https://example.com/photos/sneakers.jpg"
+    },
+    {
+        "name": "Casual Shirt",
+        "category": "Clothing",
+        "price": 1550,
+        "color": "yellow",
+        "brand": "Roadster",
+        "photo": "https://example.com/photos/casual_shirt2.jpg"
+    },
+    {
+        "name": "Running Shoes",
+        "category": "Footwear",
+        "price": 2750,
+        "color": "blue",
+        "brand": "HRX by Hrithik Roshan",
+        "photo": "https://example.com/photos/running_shoes2.jpg"
+    },
+    {
+        "name": "Formal Pants",
+        "category": "Clothing",
+        "price": 2150,
+        "color": "green",
+        "brand": "Friskers",
+        "photo": "https://example.com/photos/formal_pants2.jpg"
+    },
+    {
+        "name": "Leather Jacket",
+        "category": "Clothing",
+        "price": 4650,
+        "color": "black",
+        "brand": "Tommy Hilfiger",
+        "photo": "https://example.com/photos/leather_jacket2.jpg"
+    },
+    {
+        "name": "Wrist Watch",
+        "category": "Accessories",
+        "price": 3250,
+        "color": "blue",
+        "brand": "V-Mart",
+        "photo": "https://example.com/photos/wrist_watch2.jpg"
+    },
+    {
+        "name": "Sports Cap",
+        "category": "Accessories",
+        "price": 975,
+        "color": "gray",
+        "brand": "WROGN",
+        "photo": "https://example.com/photos/sports_cap2.jpg"
+    },
+    {
+        "name": "Denim Jeans",
+        "category": "Clothing",
+        "price": 2450,
+        "color": "navy blue",
+        "brand": "U.S. Polo Assn.",
+        "photo": "https://example.com/photos/denim_jeans2.jpg"
+    },
+    {
+        "name": "Sneakers",
+        "category": "Footwear",
+        "price": 3799,
+        "color": "red",
+        "brand": "VIMAL JONNEY",
+        "photo": "https://example.com/photos/sneakers2.jpg"
+    },
+    {
+        "name": "Casual Shirt",
+        "category": "Clothing",
+        "price": 1700,
+        "color": "green",
+        "brand": "Roadster",
+        "photo": "https://example.com/photos/casual_shirt3.jpg"
+    },
+    {
+        "name": "Running Shoes",
+        "category": "Footwear",
+        "price": 2950,
+        "color": "yellow",
+        "brand": "HRX by Hrithik Roshan",
+        "photo": "https://example.com/photos/running_shoes3.jpg"
+    },
+    {
+        "name": "Formal Pants",
+        "category": "Clothing",
+        "price": 2225,
+        "color": "blue",
+        "brand": "Friskers",
+        "photo": "https://example.com/photos/formal_pants3.jpg"
+    },
+    {
+        "name": "Leather Jacket",
+        "category": "Clothing",
+        "price": 4780,
+        "color": "pink",
+        "brand": "Tommy Hilfiger",
+        "photo": "https://example.com/photos/leather_jacket3.jpg"
+    },
+    {
+        "name": "T-Shirt",
+        "category": "Clothing",
+        "price": 950,
+        "color": "blue",
+        "brand": "Roadster",
+        "photo": "https://example.com/photos/tshirt.jpg"
+    },
+    {
+        "name": "Loafers",
+        "category": "Footwear",
+        "price": 3450,
+        "color": "black",
+        "brand": "U.S. Polo Assn.",
+        "photo": "https://example.com/photos/loafers.jpg"
+    },
+    {
+        "name": "Backpack",
+        "category": "Accessories",
+        "price": 2200,
+        "color": "red",
+        "brand": "V-Mart",
+        "photo": "https://example.com/photos/backpack.jpg"
+    },
+    {
+        "name": "Sweatshirt",
+        "category": "Clothing",
+        "price": 1650,
+        "color": "gray",
+        "brand": "Friskers",
+        "photo": "https://example.com/photos/sweatshirt.jpg"
+    },
+    {
+        "name": "Boots",
+        "category": "Footwear",
+        "price": 2899,
+        "color": "brown",
+        "brand": "Tommy Hilfiger",
+        "photo": "https://example.com/photos/boots.jpg"
+    },
+    {
+        "name": "Belt",
+        "category": "Accessories",
+        "price": 500,
+        "color": "black",
+        "brand": "HRX by Hrithik Roshan",
+        "photo": "https://example.com/photos/belt.jpg"
+    },
+    {
+        "name": "Sunglasses",
+        "category": "Accessories",
+        "price": 1299,
+        "color": "blue",
+        "brand": "WROGN",
+        "photo": "https://example.com/photos/sunglasses.jpg"
+    },
+    {
+        "name": "Chinos",
+        "category": "Clothing",
+        "price": 1999,
+        "color": "khaki",
+        "brand": "VIMAL JONNEY",
+        "photo": "https://example.com/photos/chinos.jpg"
+    },
+    {
+        "name": "Flip Flops",
+        "category": "Footwear",
+        "price": 450,
+        "color": "green",
+        "brand": "Roadster",
+        "photo": "https://example.com/photos/flip_flops.jpg"
+    },
+    {
+        "name": "Watch",
+        "category": "Accessories",
+        "price": 2999,
+        "color": "silver",
+        "brand": "U.S. Polo Assn.",
+        "photo": "https://example.com/photos/watch.jpg"
+    },
+    {
+        "name": "Scarf",
+        "category": "Accessories",
+        "price": 899,
+        "color": "pink",
+        "brand": "V-Mart",
+        "photo": "https://example.com/photos/scarf.jpg"
+    },
+    {
+        "name": "Suit",
+        "category": "Clothing",
+        "price": 4999,
+        "color": "black",
+        "brand": "Tommy Hilfiger",
+        "photo": "https://example.com/photos/suit.jpg"
+    },
+    {
+        "name": "Running Shoes",
+        "category": "Footwear",
+        "price": 3299,
+        "color": "blue",
+        "brand": "HRX by Hrithik Roshan",
+        "photo": "https://example.com/photos/running_shoes4.jpg"
+    },
+    {
+        "name": "Cardigan",
+        "category": "Clothing",
+        "price": 2200,
+        "color": "gray",
+        "brand": "Friskers",
+        "photo": "https://example.com/photos/cardigan.jpg"
+    },
+    {
+        "name": "Hiking Boots",
+        "category": "Footwear",
+        "price": 3650,
+        "color": "brown",
+        "brand": "WROGN",
+        "photo": "https://example.com/photos/hiking_boots.jpg"
+    },
+    {
+        "name": "Leather Belt",
+        "category": "Accessories",
+        "price": 1100,
+        "color": "black",
+        "brand": "VIMAL JONNEY",
+        "photo": "https://example.com/photos/leather_belt.jpg"
+    },
+    {
+        "name": "Dress Shirt",
+        "category": "Clothing",
+        "price": 1750,
+        "color": "white",
+        "brand": "U.S. Polo Assn.",
+        "photo": "https://example.com/photos/dress_shirt.jpg"
+    },
+    {
+        "name": "Ankle Boots",
+        "category": "Footwear",
+        "price": 4500,
+        "color": "black",
+        "brand": "Tommy Hilfiger",
+        "photo": "https://example.com/photos/ankle_boots.jpg"
+    },
+    {
+        "name": "Sweater",
+        "category": "Clothing",
+        "price": 1450,
+        "color": "green",
+        "brand": "Roadster",
+        "photo": "https://example.com/photos/sweater.jpg"
+    },
+    {
+        "name": "Trainers",
+        "category": "Footwear",
+        "price": 3100,
+        "color": "blue",
+        "brand": "HRX by Hrithik Roshan",
+        "photo": "https://example.com/photos/trainers.jpg"
+    },
+    
+]
