317ed499caa7639be46eee5ff4fc66b076df76c1
theeasylearn <theeasylearn@gmail.com>
2024-09-23 09:35:56 +0530

    api

--- nodejs/mongodb/api.js
+++ nodejs/mongodb/api.js
@@ -16,23 +16,62 @@ app.post(ROUTE,function(request,response){
     dbPromise.then((database) => {
         let data = request.body;
         console.log(data);
-        database.collection('easylearn').insertOne(data);
-        response.json([{ 'error': 'no', 'message': 'object added' }]);
+        database.collection('easylearn').insertOne(data,function(error,result){
+            if(error)
+                response.json([{'error':'error in updating document'}]);
+            else 
+                response.json([{'error':'no'},{'message':'document inserted'}]);
+        });
 
     }).catch((error) => {
-        response.json([{ 'error': 'yes', 'message': 'error in connection' }]);
+        response.json([{ 'error': 'error in connection' }]);
     });
 });
 
 
 //it is used to update existing person
 app.put(ROUTE,function(request,response){
-
+    let {name,pincode,address} = request.body;
+    if(name === undefined || pincode === undefined || address === undefined)
+    {
+        response.json([{'error':'input is missing'}]);
+    }    
+    else 
+    {
+        dbPromise.then((database) => {
+            let condition = {name : name};
+            let newvalue = { $set : 
+                {
+                    address: address,
+                    pincode: pincode
+                }
+            };
+            database.collection('easylearn').updateOne(condition,newvalue,function(error,result){
+                if(error)
+                    response.json([{'error':'error in updating document'}]);
+                else 
+                    response.json([{'error':'no'},{'message':'document updated'}]);
+            });
+        }).catch((error) => {
+            response.json([{ 'error': 'error in connection'}]);
+        })
+    }
 });
 
 //it is used to delete  person
 app.delete(ROUTE,function(request,response){
-
+    
+    dbPromise.then((database) => {
+        var condition = {subject:'mongodb'};
+        database.collection('easylearn').deleteOne(condition,function(error,result){
+            if(error)
+                response.json([{'error':'error in deleting document'}]);
+            else 
+                response.json([{'error':'no'},{'message':'document deleted'}]);
+        });
+    }).catch((error) => {
+        response.json([{ 'error': 'error in connection'}]);
+    })
 });
 
 
